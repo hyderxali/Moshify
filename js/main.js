e73a017ad304f1d98b917bf2b5dd67fcf402362a
hyder ali <alixhussain3@gmail.com>
2024-06-22 21:18:28 +0500

    built responsive navbar component and specify the click event only for toggler button in navbar and collapsing component in main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-document.querySelectorAll(".collapsible").forEach((collapsible) => {
-    collapsible.addEventListener("click", function (e) {
-
-        this.classList.toggle("collapsible--collapsed");
+document.querySelectorAll(".icon-container").forEach((icon) => {
+    icon.addEventListener("click", function (e) {
+        let collapsible = icon;
+        while (collapsible.parentNode.classList.contains("collapsible") != true) {
+            collapsible = collapsible.parentNode;
+        }
+        collapsible = collapsible.parentNode;
+        collapsible.classList.toggle("collapsible--expanded");
     })
 }
 );
