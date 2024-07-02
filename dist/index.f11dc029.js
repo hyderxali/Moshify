e0f66bb2e8ddaee6533ef111d479945a651ad91f
hyder ali <alixhussain3@gmail.com>
2024-07-02 21:57:15 +0500

    fixed layout and build parcel.

---
+++ dist/index.f11dc029.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".icon-container").forEach(e=>{e.addEventListener("click",function(l){let n=e;for(;!0!=n.parentNode.classList.contains("collapsible");)n=n.parentNode;(n=n.parentNode).classList.toggle("collapsible--expanded")})});
+//# sourceMappingURL=index.f11dc029.js.map
